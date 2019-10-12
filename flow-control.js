12ae17a196f5bddbedaf48c482f6f7377f7cf952
João Pedro Enes da Silva Barbedo Barreto <eng.joao.barreto@gmail.com>
2019-10-12 16:44:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,13 +1,25 @@
 function basicTeenager(age) {
-
+if (age >= 13 && age <=19) {
+  console.log("You are a teenager!")
+} 
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <=19) {
+    console.log("You are a teenager!")
+  } else {
+    console.log("You are not a teenager")
+  }
 }
 
 function ageChecker(age) {
-
+if (age <= 12){
+  console.log("You are a kid");
+}else if (age >=20) {
+  console.log("You are a grownup");
+}else {
+  console.log("You are a teenager!");
+}
 }
 
 function ternaryTeenager(age) {
